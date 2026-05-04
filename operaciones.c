e3c015779501cecd0d44688641c44bdf5f9c2ee9
fabriziosanabriapaez <fsanabriapez@frba.utn.edu.ar>
2026-05-04 17:14:36 -0300

    Agrega comentario explicativo en multiplicar

--- operaciones.c
+++ operaciones.c
@@ -15,6 +15,7 @@ int restar(int a, int b) {
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 
 /* Devuelve el producto entre dos enteros*/
+// Descarte de void
 
 int multiplicar(int a, int b) {
     return a * b;
