34aa7f88ca7b8b252f11754ad72d13a3a3e61a7e
Matias Apaza Llanos <mapazallanos@frba.utn.edu.ar>
2026-05-04 17:37:44 -0300

    Atiende review: agrego const a los parametros de la funcion multiplicar

--- operaciones.c
+++ operaciones.c
@@ -17,7 +17,7 @@ int restar(int a, int b) {
 /* Devuelve el producto entre dos enteros*/
 // Descarte de void
 
-int multiplicar(int a, int b) {
+int multiplicar(const int a, const int b) {
     return a * b;
 }
 
